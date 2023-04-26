4bdac8aeebb28fc21f28d7842e2d62cb3940daf9
cjy9249 <cjy920409@gmail.com>
2023-04-26 18:20:11 +0900

    feat-newProject

---
+++ src/main/java/com/release_server/ReleaseServerApplication.java
@@ -0,0 +1,14 @@
+package com.release_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseServerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        SpringApplication.run(ReleaseServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/release_server/ReleaseServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.release_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
